2045a2f829214145bd61d608d6736314977192dd
AndrewFan0408 <andrewfan0408@gmail.com>
2022-11-10 19:48:41 -0800

    Pass startsWith

--- practice.js
+++ practice.js
@@ -61,7 +61,9 @@ var onlyOneFruit = function (fruits, targetFruit) {
 // use _.filter to return the fruits array with only fruits
 // starting with the letter 'P'.
 var startsWith = function (fruits, letter) {
-
+  return _.filter(fruits, function(fruit) {
+    return fruit.slice(0, 1) === letter;
+  })
 };
 
 // return a filtered array containing only cookie-type desserts.
